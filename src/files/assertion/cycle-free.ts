0528908738ff22a6173ba8f75a4795054bb6737f
niesselu <lukas.niessen@ista.com>
2025-06-02 19:47:46 +0200

    Updated README

--- src/files/assertion/cycle-free.ts
+++ src/files/assertion/cycle-free.ts
@@ -23,21 +23,31 @@ export const gatherCycleViolations = (
 
 	console.log('preconditionPatterns:', preconditionPatterns);
 
+	// Check for empty test if no edges match preconditions
+	/**
+	 * Important note. Empty checks are a very difficult topic for cycle free checks.
+	 *
+	 * Example: folder A has one file, but that file has only one dependency to folder B.
+	 * So when we say .inFolder("A").should().haveNoCycles() we don't want to consider the file
+	 * since we want to check for cycles inside of folder A only. But if we report an
+	 * EmptyTest error, the users are puzzled as the folder has a file.
+	 *
+	 * So we go the non ideal way of just checking the unfiltered ones for being empty or not.
+	 */
+	if (projectedEdges.length === 0 && !options?.allowEmptyTests) {
+		return [new EmptyTestViolation(preconditionPatterns)];
+	}
+
 	const filteredEdges = projectedEdges.filter(
 		(edge) =>
 			// Exclude self-referencing edges (these are added for completeness but aren't considered cycles here)
 			edge.sourceLabel !== edge.targetLabel &&
-			(matchingAllPatterns(edge.sourceLabel, preconditionPatterns) ||
-				matchingAllPatterns(edge.targetLabel, preconditionPatterns))
+			matchingAllPatterns(edge.sourceLabel, preconditionPatterns) &&
+			matchingAllPatterns(edge.targetLabel, preconditionPatterns)
 	);
 	filteredEdges.forEach((edge) =>
 		logger.info(`Edge under check: From ${edge.sourceLabel} to ${edge.targetLabel}`)
 	);
-
-	// Check for empty test if no edges match preconditions
-	if (filteredEdges.length === 0 && !options?.allowEmptyTests) {
-		return [new EmptyTestViolation(preconditionPatterns)];
-	}
 
 	const projectedCycles = projectCycles(filteredEdges);
 
